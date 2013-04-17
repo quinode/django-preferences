a9d1b1a23e79bdadef1ebbd31b4d69236cbd846c
Dominique Guardiola <dguardiola@quinode.fr>
2013-04-17 16:55:49 +0200

    remove deps

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     ],
     include_package_data=True,
     install_requires = [
-        'Django',
+#        'Django',
     ],
     classifiers=[
         "Programming Language :: Python",
